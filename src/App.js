67cd4f3131d8dd13c5e220b2a309032251c7853c
Ravi Teja <ravikanna183@gmail.com>
2023-04-23 13:39:03 +0100

    Updated neccessary files and code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }
